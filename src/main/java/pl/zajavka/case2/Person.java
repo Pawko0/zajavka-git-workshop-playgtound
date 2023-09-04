4980606048eb3eccef80c76d1f817cf5035d3866
Pawel Mitko <pawel@zajavka.pl>
2023-09-04 13:59:16 +0200

    Lemon Haze

--- src/main/java/pl/zajavka/case2/Person.java
+++ src/main/java/pl/zajavka/case2/Person.java
@@ -5,7 +5,9 @@ import java.math.BigDecimal;
 record Person(String name, Long age, BigDecimal salary) {
 
     public Boolean doYouHaveABike() {
+        System.out.println("Lemon Haze");
         return true;
+
     }
 
     public Boolean doYouLikePizza() {
